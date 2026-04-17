acdd1f43b7cc79f61a06ed8c19a1216774a478cd
Ariel Salminen <info@arielsalminen.com>
2026-04-17 22:46:52 +0300

    [docs]: Remove tracking script

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -88,19 +88,6 @@ export default defineConfig({
     ["link", { rel: "icon", href: "/favicon.svg", sizes: "any", type: "image/svg+xml" }],
     ["link", { rel: "apple-touch-icon", href: "/apple-touch-icon.png" }],
     ["link", { rel: "manifest", href: "/manifest.json" }],
-    ...(noIndex
-      ? []
-      : [
-          [
-            "script",
-            {
-              src: "https://cdn.usefathom.com/script.js",
-              "data-spa": "auto",
-              "data-site": "RRTFEBPA",
-              defer: "",
-            },
-          ],
-        ]),
   ],
   markdown: {
     headers: true,
